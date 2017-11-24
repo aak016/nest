f6648a2d0e0a6ca4cbc0b1e1ab641d8d713de886
Alexey Kondakov <alexey.kondakov@thomsonreuters.com>
2017-11-24 21:03:29 +0300

    Creating an empty app

---
+++ NestApp/NestApp/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  NestApp
+//
+//  Created by Alexey Kondakov on 24/11/2017.
+//  Copyright © 2017 aak016. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
